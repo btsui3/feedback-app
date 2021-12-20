74a46729bf2465c8491d7cc0b06b2a4f9b5952eb
Ben Tsui <ben@bentsui.dev>
2021-12-20 15:07:10 -0500

    fix js error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 function App() {
     return (
-        <h1>Hello from the app component</h1>
-        <p>this is going to fail hopefully</p>
-    )
+        <div>
+            <h1>Hello from the app component</h1>
+            <p>this is going to fail hopefully</p>
+        </div>
+        )
 }
 
 export default App
